141c9e34c698c904c79e34ada5020b2edfe4c26b
Lars Andersson <lars.j.andersson@chalmers.se>
2016-01-18 13:47:38 +0100

    Update database on deployment.

--- ChalmersxTools/Global.asax.cs
+++ ChalmersxTools/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChalmersxTools.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace ChalmersxTools
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            SqlClientConfig.EnableCodeFirstMigrations();
         }
     }
 }
