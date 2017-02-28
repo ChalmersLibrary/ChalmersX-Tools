24450251964ddc7d1fcb08c50fbe8fa65848d144
Lars Andersson <lars.j.andersson@chalmers.se>
2017-02-28 16:22:15 +0100

    Better date format in CSV.

--- ChalmersxTools/Tools/SingleTemperatureMesaurementTool.cs
+++ ChalmersxTools/Tools/SingleTemperatureMesaurementTool.cs
@@ -73,11 +73,11 @@ namespace ChalmersxTools.Tools
                 data += "\"" + submission.Position.Latitude + "\",\"" +
                     submission.Position.Longitude + "\",\"" +
                     submission.Measurement + "\",\"" +
-                    submission.Time + "\",\"" +
+                    submission.Time.ToString("yyyy-MM-dd HH:mm") + "\",\"" +
                     submission.StationPosition.Latitude + "\",\"" +
                     submission.StationPosition.Longitude + "\",\"" +
                     submission.StationMeasurement + "\",\"" +
-                    submission.StationTime + "\"\n";
+                    submission.StationTime.ToString("yyyy-MM-dd HH:mm") + "\"\n";
             }
 
             return new CsvFileData(_session.ContextId + "-temperature-measurements.csv",
